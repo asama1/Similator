6c177f2481837f6ce56e7cbc319578af0736b4cd
asama1 <79220179+asama1@users.noreply.github.com>
2022-06-08 09:21:09 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,22 @@
+
+
+//let sbot = "startButton.js"
+let sbot;
+let a;
+let zlick
+zlick = 220
+function setup() {
+  createCanvas(400, 400);
+  a = new thing();
+   sbot= new Button();
+}
+
+function draw() {
+  background(zlick);
+  
+ 
+  sbot.show();
+  sbot.update();
+  
+  
+}
