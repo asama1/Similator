627a72a4f475300ecca0c0e8f695775f5a603728
alant4 <100594043+alant4ocdsb@users.noreply.github.com>
2022-06-09 11:01:11 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 
-
+let c=0;
 //let sbot = "startButton.js"
 let sbot;
 let a;
@@ -8,15 +8,20 @@ zlick = 220
 function setup() {
   createCanvas(400, 400);
   a = new thing();
-   sbot= new Button();
+   sbot= new Button(c);
 }
 
 function draw() {
-  background(zlick);
   
- 
-  sbot.show();
+  background(0);
+  if (sbot.c<1){
+    sbot.show();
   sbot.update();
+  } else{
+  background(145);
+  }
   
   
 }
+
+
