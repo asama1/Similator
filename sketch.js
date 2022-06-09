3482da3c59cf38307b33d63b3afebb4963662858
asama1 <79220179+asama1@users.noreply.github.com>
2022-06-09 11:01:48 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,14 @@
 
-
 //let sbot = "startButton.js"
 let sbot;
 let a;
 let zlick
+let wordso
+wordso = "Hello"
+let line1 = ""
+let line2 = ""
+let line3 = ""
+let line4 = ""
 zlick = 220
 function setup() {
   createCanvas(400, 400);
@@ -13,10 +18,15 @@ function setup() {
 
 function draw() {
   background(zlick);
-  
- 
+  text(wordso, 10, 320)
+ text(line1, 10, 335)
+  text(line2, 10, 350)
+  text(line3, 10, 365)
+  text(line4, 10, 380)
   sbot.show();
   sbot.update();
   
   
 }
+
+
