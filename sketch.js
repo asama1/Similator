e3e36ef235fc433caa61c9c24a100b883cdd8f21
Finleysp <100596671+Finleysp@users.noreply.github.com>
2022-06-17 10:54:08 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,22 +1,46 @@
 
-
-//let sbot = "startButton.js"
-let sbot;
+let c=0;
+let sbot = "startButton.js";
+let wordso; 
 let a;
-let zlick
-zlick = 220
+let zlick;
+zlick = 220;
+let bonk;
+
+wordso = "Hello"
+let line1 = ""
+let line2 = ""
+let line3 = ""
+let line4 = ""
+function preload(){
+  img = loadImage('bufftbh.jpg')
+}
 function setup() {
   createCanvas(400, 400);
-  a = new thing();
-   sbot= new Button();
+  bonk = new haracters()
+   sbot= new Button(c);
 }
 
 function draw() {
-  background(zlick);
-  
- 
-  sbot.show();
+
+  background(0);
+  // gets rid of the button entirely
+  if (sbot.c<1){
+    sbot.show();
   sbot.update();
+  } else {
+  background(145);
+  image(img,-100,0);
+    rect(4, 300, 390, 95)
+        text(wordso, 10, 320)
+ text(line1, 10, 335)
+  text(line2, 10, 350)
+  text(line3, 10, 365)
+text(line4, 10, 380)
+    bonk.show()
+    bonk.update()
+rect(150, 150, 150, 25);
+    textSize(16);
+    text("Wanna hit the gym?", 152,170);
+  }
   
-  
-}
