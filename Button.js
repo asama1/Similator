dd61850db2bd2ab9d6af35f74b017462d67d3136
Finleysp <100596671+Finleysp@users.noreply.github.com>
2022-06-19 15:52:27 -0400

    Update Button.js

--- Button.js
+++ Button.js
@@ -1,29 +1,25 @@
-class Button{
-  constructor(){
-    
-  }
-  
-  show(){
-    
-   
-     rect(153,160, 80, 50)
-     text("START", 175, 190)
-    
-  }
-  
-  
-  update(){
-   if (mouseX > 160 && mouseX < 210 && mouseY > 180 && mouseY < 210 && mouseIsPressed === true){
-   //  fill(1000)
-    // text('hello', 50, 50)
-     zlick = 0
-     console.log('mouse be clicked')
-     blackOut();
-     removeElements();
-}
-  }
-}
-function blackOut(){
-  fill(zlick);
-}
 
+class Button {
+  constructor(x) {
+    this.c = x;
+  }
+
+  show() {
+    rect(153, 160, 80, 50);
+    text("START", 175, 190);
+  }
+
+  update() {
+    if (
+      mouseX > 160 &&
+      mouseX < 210 &&
+      mouseY > 180 &&
+      mouseY < 210 &&
+      mouseIsPressed === true
+    ) {
+      this.c = this.c + 1;
+      zlick = 0;
+      console.log(this.c);
+    }
+  }
+}
